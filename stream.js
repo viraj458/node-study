e065ca1d24fd8664e2f87c74fa975f7fc623e0d1
Viraj <virajmalaka94@gmail.com>
2022-12-21 21:40:34 +0530

    piping

--- stream.js
+++ stream.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/docs2.txt', {encoding: 'utf-8'});
-const writeStream = fs.createWriteStream('./docs/docs3.txt');
+const writeStream = fs.createWriteStream('./docs/docs4.txt');
 
 readStream.on('data', (chunk) => {
     console.log('---CHUNK---');
     console.log(chunk);
 
-    writeStream.write('\nNEW CHUK\n')
+    writeStream.write('\nNEW CHUK\n');
     writeStream.write(chunk);
 })
+
+//piping
+readStream.pipe(writeStream);

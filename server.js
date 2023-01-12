e94faee82f5ddcf469b0e0af1c5fbeb3fb919be2
Viraj <virajmalaka94@gmail.com>
2023-01-12 15:49:12 +0530

    Sending html pages

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log('request was made')
+    console.log(req.url, req.method);
+
+    // //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('<p>Hello</p>');
+    // res.write('<p>hello hello</p>')
+
+    // res.end()
+
+    //send html file
+    fs.readFile('./view/index.html', (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            // res.write(data);
+            // console.log(data.toString());
+            res.end(data);
+        }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
